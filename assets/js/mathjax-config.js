31bc1edaa7854651452c4b69e70306783f5a762d
y1zhou <zhou.zy.yi@gmail.com>
2020-03-05 12:33:36 -0500

    fix(mathjax): fix display-mode MathJax

---
+++ assets/js/mathjax-config.js
@@ -0,0 +1,23 @@
+// MathJax Configuration
+//
+// v2 to v3 upgrade notes:
+// - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
+// - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
+window.MathJax = {
+  loader: {
+    load: ["output/svg", "[tex]/noerrors"]
+  },
+  tex: {
+    inlineMath: [["$", "$"]],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"]
+    ],
+    tags: "ams",
+    processEscapes: true,
+    packages: { "[+]": ["noerrors"] }
+  },
+  chtml: {
+    scale: 0.8
+  }
+};
