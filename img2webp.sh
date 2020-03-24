5800f0b784520f621db1b908ebb5c7a8f544e052
y1zhou <zhou.zy.yi@gmail.com>
2020-03-24 17:22:51 -0400

    fix(footer): change web host

--- img2webp.sh
+++ img2webp.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 # Convert gif animations to the webp format
-find . -name "*.gif" -type f -exec gif2webp {} -o {}.webp \;
-find . -name "*.webp" -exec rename 's/(.*)\.gif\.webp/$1.webp/' {} \;
-find . -name "*.gif" -type f -delete
-find . -name "*.gif.webp" -type f -delete  # in case of duplicates
+find ./content -name "*.gif" -type f -exec gif2webp {} -o {}.webp \;
+find ./content -name "*.webp" -exec rename 's/(.*)\.gif\.webp$/$1.webp/' {} \;
+find ./content -name "*.gif" -type f -delete
+find ./content -name "*.gif.webp" -type f -delete  # in case of duplicates
